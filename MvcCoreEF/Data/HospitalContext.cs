183f96f41f4b049fbb524e310a86e3af2a2b7047
CPenalba <carolinapenalbacorpa@gmail.com>
2025-02-12 10:04:18 +0100

    Add project files.

---
+++ MvcCoreEF/Data/HospitalContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreEF.Models;
+
+namespace MvcCoreEF.Data
+{
+    public class HospitalContext: DbContext
+    {
+        //TENDREMOS UN CONSTRUCTOR QUE RECIBA LAS OPCIONES DE INICIO PARA EL CONTEXT, COMO LA CADENA DE CONEXION POR EJEMPLO
+        public HospitalContext(DbContextOptions<HospitalContext> options) : base(options) 
+        { }
+        //EN ESTA CLASE ESTARAN LAS COLECCIONES DE LOS MODELOS QUE SERAN LAS QUE UTILIZAREMOS MEDIANTE LINQ
+        public DbSet<Hospital> Hospitales { get; set; }
+    }
+}
